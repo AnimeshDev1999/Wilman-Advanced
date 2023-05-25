51c03cb169a1441b742ca77244e48a95b6847660
Animesh <animesh99dev@gmail.com>
2023-05-25 22:23:36 +0530

    added faq section

--- src/App.js
+++ src/App.js
@@ -2,14 +2,41 @@ import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
 import GridBox from "./components/GridBox";
 import Footer from "./components/Footer";
+import ApplyForm from "./components/ApplyForm";
+import Fran from "./components/Fran";
+import Login from "./components/Login";
+import Faq from "./components/FAQ";
+import { useState } from "react";
 
 function App() {
+  const [page, setPage] = useState("Home");
+  const pageSwitch = (e) => {
+    setPage(e.target.innerText);
+  };
   return (
     <div className="relative">
-      <NavBar></NavBar>
-      <Hero></Hero>
-      <GridBox></GridBox>
+      <NavBar func={pageSwitch}></NavBar>
+      {page === "Home" ? (
+        <>
+          <Hero></Hero>
+          <GridBox></GridBox>
+        </>
+      ) : page === "Franchisees" ? (
+        <Fran func={pageSwitch}></Fran>
+      ) : page === "Apply Now" ? (
+        <ApplyForm></ApplyForm>
+      ) : page === "Login" ? (
+        <Login></Login>
+      ) : page === "FAQ" ? (
+        <Faq></Faq>
+      ) : (
+        ""
+      )}
       <Footer></Footer>
+      {/* <Hero></Hero>
+      <GridBox></GridBox>
+      <Fran></Fran>
+      <ApplyForm></ApplyForm> */}
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,27 @@
 import logo from "../images/logoAlpha.png";
 
-const NavBar = () => {
+const NavBar = (props) => {
   return (
-    <div className="absolute top-0 left-0 w-full flex justify-between items-center px-10 py-5">
+    <div className="fixed mt-5 top-0 left-0 w-full flex justify-between items-center px-10 py-5">
       <img
         src={logo}
         className="h-14 bg-[rgba(255,255,255,0.75)] rounded-md"
         alt=""
       />
       <div className="bg-[rgba(255,255,255,0.75)] rounded-md flex gap-5 justify-center items-center px-5 py-3">
-        <button className="nav-btn">Services</button>
-        <button className="nav-btn">Investors</button>
-        <button className="bg-WPurple hover:bg-WGold duration-500 text-white px-3 py-1 rounded-md">
+        <button onClick={props.func} className="nav-btn">
+          Home
+        </button>
+        <button onClick={props.func} className="nav-btn">
+          Franchisees
+        </button>
+        <button onClick={props.func} className="nav-btn">
+          FAQ
+        </button>
+        <button
+          onClick={props.func}
+          className="bg-WPurple hover:bg-WGold duration-500 text-white px-3 py-1 rounded-md"
+        >
           Login
         </button>
       </div>
